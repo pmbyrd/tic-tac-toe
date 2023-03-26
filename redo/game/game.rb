56993bd9a72a43db82e1f98e35f6c73e7f491381
Perriesha <pmbyrd@gmail.com>
2023-03-25 21:40:30 -0700

    Tuning logic

--- redo/game/game.rb
+++ redo/game/game.rb
@@ -4,39 +4,33 @@ require_relative 'game_modules'
 
 class Game
     include GameModules
-    #In the class is where ther terminal interaction will be handled
-    #The game class will be responsible for the game loop
-    #The game class will be responsible for the game logic
-    #The game class will be responsible for the game flow
-    #The game class will be responsible for the game state
-    #The game class will be responsible for the game over
-    #The game class will be responsible for the game win
-    #The game class will be responsible for the game tie
-    #The game class will be responsible for the game reset
-    #The game class will be responsible for the game start
+
     #*A game will have two players and board
-    #The game is what will be run in the terminal
     attr_accessor :player1, :player2, :board
     def initialize
         @player1 = Player.new("X")
         @player2 = Player.new("O")
         @board = Board.new(3)
         @board.enumerate_board
-        @board.display_board
     end
 end
 
 
 tic_tac_toe = Game.new
-board = tic_tac_toe.board.board
+X = tic_tac_toe.player1
+O = tic_tac_toe.player2
+
 p tic_tac_toe.player1.marker
 p tic_tac_toe.player2.marker
+board = tic_tac_toe.board
 puts
-tic_tac_toe.check_tie(board)
+p board.class
+
 puts
-p board
-puts
-p board.include?(1)
-#The board needs to be flattened to check for numbers
-p board.flatten.include?(2)
-p player1.get_player_input
+p board.display_board
+p board.check_board(1, board.board.flatten)
+p board.check_board(10, board.board.flatten)
+#*if a puts is the last line AKA the return statement then we will return nil
+p board.check_winning_combo
+# p board.update_board(board.board, 1, "X")
+p tic_tac_toe.switch_player(X)
--- redo/game/game_modules.rb
+++ redo/game/game_modules.rb
@@ -1,16 +1,12 @@
 module GameModules
   #Modules for game functionality
-  winner = false
-  def check_winner
-    #Check if there is a winner
-    #If there is a winner, display the winner
-    #If there is no winner, continue the game
-    puts "Checking for a winner"
-  end
+    winner = false
+    current_player 
+    def check_winner(board)
+        puts "Checking for a winner"
+    end
+
     def check_tie(board)
-        #Check if there is a tie
-        #If there is a tie, display the tie
-        #If there is no tie, continue the game
         unless board.include?(Numeric) && winner == false
             puts "Continue the game"
             puts board.include?(Numeric)
@@ -19,46 +15,60 @@ module GameModules
         end
         puts "Checking for a tie"
     end
+
     def play_game
-        #Play the game
-        #Check for a winner
-        #Check for a tie
-        #If there is a winner or tie, end the game
-        #If there is no winner or tie, continue the game
         #*While there is no winner or tie, continue the game that is how the game loop will be implemented
         puts "Playing the game"
     end
-    def check_winning_combo
-        #Check for a winning combination
-        #If there is a winning combination, display the winning combination
-        #If there is no winning combination, continue the game
-        puts "Checking for a winning combination"
-        
+    def switch_player(current_player)
+        puts "Switching players"
+        current_player == X ? current_player = O : current_player = X
     end
+
 end
 
 module BoardModules
+    #Modules for board functionality
     def update_board(board, player_input, player_marker)
-        #Update the board with the player's input
-        #If the player's input is valid, update the board
-        #If the player's input is invalid, ask for a new input
         puts "Please enter a number to place your piece"
+    end
+
+    def check_winning_combo
+        puts "Checking for a winning combination"
+        winning_combos = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9],
+            [1,4,7],
+            [2,5,8],
+            [3,6,9],
+            [1,5,9],
+            [3,5,7]
+        ]    
+    end
+    
+    def check_board(player_selection, board)
+        puts "Checking if the player's input is on the board"
+        unless board.include?(player_selection)
+            puts "Please enter a number on the board"
+        end
     end
 end
 
 module PlayerModules
     #Get the player's input to be used throughout the game
     def get_player_input
-        result = gets.chomp
+        response = gets.chomp
     end
+
     def add_selection(player_selection)
-        @selections << player_selection
+        @selections << player_selection     
     end
+
     def check_selection(player_selection)
-        #Check if the player's input is valid
-        #If the player's input is valid, update the board
-        #If the player's input is invalid, ask for a new input
         puts "Checking if the player's input is valid"
-        
+        unless player_selection.is_a?(Numeric)
+            puts "Please enter a number"
+        end
     end
 end
